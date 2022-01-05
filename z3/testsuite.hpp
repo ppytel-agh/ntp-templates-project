d707950fbe5f9ba198942d09a23e4e8c97b499a2
ppytel <ppytel@student.agh.edu.pl>
2022-01-05 17:41:52 +0100

    written abstract testsuite for pascals triangle

--- z3/testsuite.hpp
+++ z3/testsuite.hpp
@@ -4,20 +4,28 @@ const char* validityString(bool validity) {
     return validity ? "valid" : "INVALID";
 }
 
-template<template<unsigned int, unsigned int> class TestedTemplate, unsigned int a, unsigned int b>
-void testCase(bool expectedValidity, int expectedValue = 0) {
-    std::cout << "TESTCASE TestedTemplate<" << a << ", " << b << ">" << std::endl;
-    std::cout << "validity: expected - " << validityString(expectedValidity) << ", actual - " << validityString(TestedTemplate<a, b>::valid) << std::endl;
-    std::cout << "value: expected - " << expectedValue << ", actual - " << TestedTemplate<a, b>::value << std::endl;
-    bool testResult = (expectedValidity == TestedTemplate<a, b>::valid) && (expectedValue == TestedTemplate<a, b>::value);
-    std::cout << "test " << (testResult ? "SUCCEEDED" : "FAILED") << std::endl << std::endl;
-}
 
-template<template<unsigned int, unsigned int> class TestedTemplate >
-void pascalsTriangleTestSuite() {
-    testCase<TestedTemplate, 1,1>(true, 1);
-    testCase<TestedTemplate, 0,1>(false);
-    testCase<TestedTemplate, 1,2>(false);
-    testCase<TestedTemplate, 3,2>(true, 2);
-    testCase<TestedTemplate, 8,3>(true, 21);
+#define GENERATE_PASCALSTRIANGLE_TESTSTUITE_TEMPLATE_FUNCTION(VALIDITY_FIELD_NAME, VALUE_FIELD_NAME, FIRST_ROW_INDEX, FIRST_COLUMN_INDEX) \
+\
+template<template<unsigned int, unsigned int> class TestedTemplate, unsigned int a, unsigned int b> \
+void testCase(bool expectedValidity, int expectedValue = 0) { \
+    std::cout << "TESTCASE TestedTemplate<" << a << ", " << b << ">" << std::endl; \
+    std::cout << "validity: expected - " << validityString(expectedValidity) << ", actual - " << validityString(TestedTemplate<a, b>::VALIDITY_FIELD_NAME) << std::endl; \
+    std::cout << "value: expected - " << expectedValue << ", actual - " << TestedTemplate<a, b>::VALUE_FIELD_NAME << std::endl; \
+    bool testResult = (expectedValidity == TestedTemplate<a, b>::VALIDITY_FIELD_NAME) && (expectedValue == TestedTemplate<a, b>::VALUE_FIELD_NAME); \
+    std::cout << "test " << (testResult ? "SUCCEEDED" : "FAILED") << std::endl << std::endl; \
+} \
+\
+template<template<unsigned int, unsigned int> class TestedTemplate > \
+void pascalsTriangleTestSuite() { \
+    const int preFirstRowIndex = FIRST_ROW_INDEX - 1; \
+    const int secondColumnIndex = FIRST_COLUMN_INDEX +  1; \
+    const int thirdRowIndex = FIRST_ROW_INDEX + 2; \
+    const int eighthRowIndex = FIRST_ROW_INDEX + 7; \
+    const int thirdColumnIndex = FIRST_COLUMN_INDEX + 2; \
+    testCase<TestedTemplate, FIRST_ROW_INDEX, FIRST_COLUMN_INDEX>(true, 1); \
+    testCase<TestedTemplate, preFirstRowIndex, FIRST_COLUMN_INDEX>(false); \
+    testCase<TestedTemplate, FIRST_ROW_INDEX, secondColumnIndex>(false); \
+    testCase<TestedTemplate, thirdRowIndex, secondColumnIndex>(true, 2); \
+    testCase<TestedTemplate, eighthRowIndex, thirdColumnIndex>(true, 21); \
 }
